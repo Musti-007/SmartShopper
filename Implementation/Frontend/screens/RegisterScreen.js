bd938c4be13f9faca433ac23733619a6cf33b624
Musti-007 <60880522+Musti-007@users.noreply.github.com>
2023-11-29 22:19:11 +0100

    add alert when and field must be filled in.

--- Implementation/Frontend/screens/RegisterScreen.js
+++ Implementation/Frontend/screens/RegisterScreen.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import axios from "axios";
 
 export default function RegisterScreen({ navigation }) {
@@ -10,9 +18,15 @@ export default function RegisterScreen({ navigation }) {
   const [repeatPassword, setRepeatPassword] = useState("");
 
   const handleRegister = () => {
+    // Check if any required field is empty
+    if (!firstName || !lastName || !email || !password || !repeatPassword) {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
+
     // Check if passwords match
     if (password !== repeatPassword) {
-      console.error("Passwords do not match");
+      Alert.alert("Error", "Passwords do not match");
       return;
     }
 
@@ -27,6 +41,9 @@ export default function RegisterScreen({ navigation }) {
         if (response.status === 201) {
           // Registration was successful (status code 201 Created)
           console.log("User registered:", response.data);
+
+          // Show alert on successful registration
+          Alert.alert("Success", "You have been successfully registered!");
 
           // Navigate to the Login screen
           navigation.navigate("Login");
@@ -78,29 +95,35 @@ export default function RegisterScreen({ navigation }) {
           secureTextEntry={true}
         />
       </View>
-      <Button title="Register" onPress={handleRegister} />
-      <Button
-        title="Already have an account? Login"
-        onPress={() => navigation.navigate("Login")}
-      />
+      <TouchableOpacity style={styles.regButton} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+
+      <View style={styles.loginContainer}>
+        <Text style={styles.logbuttonText}>Already have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.blueText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    justifyContent: "center",
-  },
+  // container: {
+  //   flex: 1,
+  //   padding: 16,
+  //   justifyContent: "center",
+  // },
   title: {
     fontSize: 40,
     marginTop: 40,
-    marginBottom: 60,
+    marginBottom: 40,
     alignSelf: "flex-start",
+    padding: 20,
   },
   inputContainer: {
-    marginBottom: 200,
+    // marginBottom: 150,
   },
   input: {
     height: 40,
@@ -109,5 +132,38 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 32,
     paddingHorizontal: 8,
+    fontSize: 16,
+    width: "94%",
+    alignSelf: "center",
+  },
+  regButton: {
+    borderRadius: 10,
+    width: "42%",
+    height: 50,
+    backgroundColor: "#6666FF",
+    marginTop: 10,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+  },
+  loginContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+  },
+  logbuttonText: {
+    textAlign: "center",
+    color: "6666FF",
+    fontSize: 16,
+  },
+  blueText: {
+    color: "#C87E61",
+    marginLeft: 5, // Add margin between "Already have an account?" and "Login"
+    fontWeight: "bold",
   },
 });
