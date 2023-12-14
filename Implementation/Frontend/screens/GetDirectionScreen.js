073c3982fd4b8d35f99cfe881b754885e9d7f6af
Musti-007 <60880522+Musti-007@users.noreply.github.com>
2023-12-14 22:01:12 +0100

    render user location on map

--- Implementation/Frontend/screens/GetDirectionScreen.js
+++ Implementation/Frontend/screens/GetDirectionScreen.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { LinearGradient } from "expo-linear-gradient";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const GetDirectionScreen = ({ route }) => {
   const { combinedData } = route.params;
+  const navigation = useNavigation();
   const mapViewRef = useRef(null);
 
   // Set initial region based on the user's location stored in AsyncStorage
   const [initialRegion, setInitialRegion] = useState({});
+  const [userLocation, setUserLocation] = useState(null);
+  const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
     const fetchUserLocation = async () => {
@@ -36,6 +42,8 @@ const GetDirectionScreen = ({ route }) => {
         const latitude = parseFloat(latStr);
         const longitude = parseFloat(lonStr);
 
+        setUserLocation({ latitude, longitude });
+
         return {
           latitude,
           longitude,
@@ -51,49 +59,101 @@ const GetDirectionScreen = ({ route }) => {
     return null;
   };
 
+  // Function to handle the "Locate Me" button press
+  const handleLocateMe = () => {
+    if (mapViewRef.current && userLocation) {
+      mapViewRef.current.animateToRegion(userLocation, 1000);
+      setCurrentRegion(userLocation);
+    }
+  };
+
+  // Function to handle region changes
+  const handleRegionChangeComplete = (region) => {
+    setCurrentRegion(region);
+  };
+
   return (
-    <View>
-      <Text style={styles.title}>Direction</Text>
-      <MapView
-        style={styles.map}
-        ref={mapViewRef}
-        initialRegion={initialRegion}
-      >
-        {combinedData.map((item, index) => {
-          // Split the "lat, lon" string from the Location key
-          const [latStr, lonStr] = item.Location.split(", ");
+    <LinearGradient
+      colors={["#371E57", "#0E1223"]}
+      style={styles.linearGradient}
+    >
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.backButton}
+        >
+          <AntDesign name="left" size={24} color="white" />
+        </TouchableOpacity>
 
-          // Convert the string values to numbers
-          const latitude = parseFloat(latStr);
-          const longitude = parseFloat(lonStr);
+        <Text style={styles.title}>Direction</Text>
 
-          return (
-            <Marker
-              key={index}
-              coordinate={{ latitude, longitude }}
-              title={item.StoreName}
-            />
-          );
-        })}
-      </MapView>
-    </View>
+        <MapView
+          style={styles.map}
+          ref={mapViewRef}
+          initialRegion={initialRegion}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          onRegionChangeComplete={handleRegionChangeComplete}
+          userLocationAnnotationTitle="Your Location"
+        >
+          <TouchableOpacity
+            onPress={handleLocateMe}
+            style={styles.locateButton}
+          >
+            <MaterialIcons name="my-location" size={24} color="black" />
+          </TouchableOpacity>
+          {combinedData.map((item, index) => {
+            // Split the "lat, lon" string from the Location key
+            const [latStr, lonStr] = item.Location.split(", ");
+
+            // Convert the string values to numbers
+            const latitude = parseFloat(latStr);
+            const longitude = parseFloat(lonStr);
+
+            return (
+              <Marker
+                key={index}
+                coordinate={{ latitude, longitude }}
+                title={item.StoreName}
+              />
+            );
+          })}
+        </MapView>
+      </View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  linearGradient: {
     flex: 1,
+  },
+  backButton: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    zIndex: 1,
   },
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginTop: 50,
-    marginBottom: 80,
+    fontSize: 40,
+    marginTop: 20,
+    marginBottom: 20,
+    alignSelf: "flex-start",
+    padding: 20,
+    color: "white",
   },
+
   map: {
-    minHeight: 700,
+    minHeight: "80%",
     flex: 1,
+    borderRadius: 10,
+  },
+  locateButton: {
+    top: 10,
+    left: 10,
+    zIndex: 1,
+    alignSelf: "flex-end",
+    paddingRight: 20,
   },
 });
 
