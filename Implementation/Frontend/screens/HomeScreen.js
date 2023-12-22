f2a3ab81ecfcafaccc8a4f8784309326f1bc5589
Musti-007 <60880522+Musti-007@users.noreply.github.com>
2023-12-22 11:36:55 +0100

    add image implementation

--- Implementation/Frontend/screens/HomeScreen.js
+++ Implementation/Frontend/screens/HomeScreen.js
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   View,
   Text,
   StyleSheet,
   FlatList,
   TouchableOpacity,
-  Image,
 } from "react-native";
 import axios from "axios";
-import { FontAwesome } from "@expo/vector-icons";
-import { useFocusEffect } from "@react-navigation/native";
-import { SearchBar } from "react-native-elements";
+import { SearchBar, Card } from "react-native-elements";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { LinearGradient } from "expo-linear-gradient";
 import * as Location from "expo-location";
@@ -19,30 +16,158 @@ function HomeScreen({ navigation }) {
   const [searchText, setSearchText] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [listOfSuperMarkets, setListOfSupermarkets] = useState([]);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // New state to track login status
-  const [bestDealProduct, setBestDealProduct] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [bestDealProducts, setBestDealProducts] = useState([]);
+  const [currentProductIndex, setCurrentProductIndex] = useState(0);
 
-  const productList = [
-    {
-      id: 1,
-      name: "Product A",
-      price: 19.99,
-      imageUri: "https://example.com/productA.jpg",
-    },
-    {
-      id: 2,
-      name: "Product B",
-      price: 29.99,
-      imageUri: "https://example.com/productB.jpg",
-    },
-    {
-      id: 3,
-      name: "Product C",
-      price: 14.99,
-      imageUri: "https://example.com/productC.jpg",
-    },
-    // Add more products as needed
-  ];
+  //login
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        // Check if the user is logged in based on the "isLoggedIn" key in AsyncStorage
+        const status = await AsyncStorage.getItem("isLoggedIn");
+        setIsLoggedIn(status === "true");
+      } catch (error) {
+        console.error("Error checking login status:", error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  //user location and supermarkets near the user
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      await AsyncStorage.setItem(
+        "location",
+        `${location.coords.latitude}, ${location.coords.longitude}`
+      );
+
+      const storedData = await AsyncStorage.getItem("supermarkets");
+
+      if (storedData) {
+        setListOfSupermarkets(JSON.parse(storedData));
+        return;
+      }
+
+      const apiUrl = `https://nominatim.openstreetmap.org/search?q=supermarket+near+${location.coords.latitude}%2C+${location.coords.longitude}&limit=40&format=json`;
+
+      try {
+        const response = await axios.get(apiUrl);
+        const storeData = response.data;
+
+        const jsonData = JSON.stringify(storeData);
+        await AsyncStorage.setItem("supermarkets", jsonData);
+        setListOfSupermarkets(JSON.parse(jsonData));
+      } catch (error) {
+        console.error("Error fetching supermarket data", error);
+      }
+    })();
+  }, []);
+
+  //
+  useEffect(() => {
+    const fetchTopCheapestProducts = async () => {
+      try {
+        // const response = await axios.get("http://192.168.1.218:3000/api/data");
+        const response = await axios.get("http://localhost:3000/api/data");
+        const allProducts = [];
+
+        // Flatten the products array from each section
+        response.data.forEach((section) => {
+          section.d.forEach((product) => {
+            allProducts.push({
+              n: product.n,
+              p: product.p,
+              i: section.i,
+              s: product.s,
+              c: section.c,
+            });
+          });
+        });
+
+        // Sort products by price in ascending order
+        const sortedProducts = allProducts.sort((a, b) => a.p - b.p);
+
+        // Get the top 10 cheapest products
+        const topCheapestProducts = sortedProducts.slice(0, 10);
+        console.log(topCheapestProducts);
+        // Fetch data for the top 10 products
+        const productsWithImages = await fetchData(topCheapestProducts);
+
+        // Set the state with the products and their images
+        setBestDealProducts(productsWithImages);
+      } catch (error) {
+        console.error("Error fetching top cheapest products:", error.message);
+      }
+    };
+
+    fetchTopCheapestProducts();
+  }, []);
+
+  useEffect(() => {
+    // Set up an interval to show a new product every 10 seconds
+    const intervalId = setInterval(() => {
+      setBestDealProducts((prevProducts) => {
+        // Calculate the next index
+        const nextIndex = (currentProductIndex + 1) % prevProducts.length;
+        // Update the current index
+        setCurrentProductIndex(nextIndex);
+        // Return the current state to avoid modifying it directly
+        return prevProducts;
+      });
+    }, 10000);
+
+    // Clean up the interval when the component is unmounted or the effect is re-run
+    return () => clearInterval(intervalId);
+
+    // Omit bestDealProducts from the dependency array
+  }, [currentProductIndex]);
+
+  const fetchData = async (newProducts) => {
+    try {
+      const promises = newProducts.map(async (item) => {
+        try {
+          const response = await axios.get(
+            // `http://192.168.1.218:3000/api/productSearch?query=${encodeURIComponent(
+            `http://localhost:3000/api/productSearch?query=${encodeURIComponent(
+              item.n
+            )}`
+          );
+
+          const imageURL =
+            response.data.productImages.length > 0
+              ? response.data.productImages[
+                  response.data.productImages.length - 1
+                ].url
+              : item.i;
+
+          return {
+            ...item,
+            imageURL: imageURL,
+          };
+        } catch (error) {
+          console.error("Error fetching data:", error.message);
+          return { ...item, imageURL: item.i };
+        }
+      });
+
+      const productsWithImages = await Promise.all(promises);
+      return productsWithImages;
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+      return newProducts;
+    }
+  };
 
   const checkIfSupermarketExists = (supermarket, section) => {
     if (
@@ -60,9 +185,8 @@ function HomeScreen({ navigation }) {
 
     const searchTextLower = text.toLowerCase();
     const filteredItems = [];
-    const data = await axios.get("http://192.168.1.218:3000/api/data");
-    // const data = await axios.get("http://localhost:3000/api/data");
-    // console.log("Data result: ", data.data);
+    // const data = await axios.get("http://192.168.1.218:3000/api/data");
+    const data = await axios.get("http://localhost:3000/api/data");
 
     for (const section of data.data) {
       if (
@@ -75,7 +199,7 @@ function HomeScreen({ navigation }) {
             filteredItems.push({
               n: product.n,
               p: product.p,
-              supermarket: section.c,
+              c: section.c,
               i: section.i,
               s: product.s,
             });
@@ -92,129 +216,23 @@ function HomeScreen({ navigation }) {
     setFilteredProducts(filteredItems);
   };
 
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      try {
-        // Check if the user is logged in based on the "isLoggedIn" key in AsyncStorage
-        const status = await AsyncStorage.getItem("isLoggedIn");
-        setIsLoggedIn(status === "true");
-      } catch (error) {
-        console.error("Error checking login status:", error);
-      }
-    };
-
-    checkLoginStatus();
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-
-      if (status !== "granted") {
-        console.log("Permission to access location was denied");
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({}); // Set user location with lat long
-
-      await AsyncStorage.setItem(
-        "location",
-        `${location.coords.latitude}, ${location.coords.longitude}`
-      ); // Check if data is already stored in AsyncStorage
-
-      const storedData = await AsyncStorage.getItem("supermarkets");
-
-      if (storedData) {
-        // Data is already stored, no need to make the request again
-        console.log("Data already exists:", storedData);
-        setListOfSupermarkets(JSON.parse(storedData));
-        return;
-      } // Fetch supermarket data using OpenStreetMap Nominatim API with axios
-
-      const apiUrl = `https://nominatim.openstreetmap.org/search?q=supermarket+near+${location.coords.latitude}%2C+${location.coords.longitude}&limit=40&format=json`;
-
-      try {
-        const response = await axios.get(apiUrl);
-        const storeData = response.data; // Convert the object to a JSON string before storing
-
-        const jsonData = JSON.stringify(storeData);
-        console.log("Setting new data");
-        await AsyncStorage.setItem("supermarkets", jsonData);
-        setListOfSupermarkets(JSON.parse(jsonData));
-      } catch (error) {
-        console.error("Error fetching supermarket data", error);
-      }
-    })();
-  }, []);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     let { status } = await Location.requestForegroundPermissionsAsync();
-
-  //     if (status !== "granted") {
-  //       console.log("Permission to access location was denied");
-  //       return;
-  //     }
-
-  //     let location = await Location.getCurrentPositionAsync({});
-
-  //     await AsyncStorage.setItem(
-  //       "location",
-  //       `${location.coords.latitude}, ${location.coords.longitude}`
-  //     );
-
-  //     const apiUrl = `https://nominatim.openstreetmap.org/search?q=supermarket+near+${location.coords.latitude}%2C+${location.coords.longitude}&limit=40&format=json`;
-
-  //     try {
-  //       const response = await axios.get(apiUrl);
-  //       const storeData = response.data;
-
-  //       const jsonData = JSON.stringify(storeData);
-  //       await AsyncStorage.setItem("supermarkets", jsonData);
-  //       setListOfSupermarkets(jsonData);
-  //     } catch (error) {
-  //       console.error("Error fetching supermarket data", error);
-  //     }
-  //   })();
-  // }, []);
-
   const handleListButtonPress = () => {
     if (isLoggedIn) {
-      navigation.navigate("Lists"); // Navigate to the ListsScreen
+      navigation.navigate("Lists");
     } else {
-      // Handle the case when the user is not logged in (e.g., show a login screen)
+      // Handle the case when the user is not logged in
       // Example: navigation.navigate('Login');
     }
   };
 
   const handleCreateListButtonPress = () => {
     if (isLoggedIn) {
-      navigation.navigate("CreateList"); // Navigate to the CreateListScreen
+      navigation.navigate("CreateList");
     } else {
-      // Handle the case when the user is not logged in (e.g., show a login screen)
+      // Handle the case when the user is not logged in
       // Example: navigation.navigate('Login');
     }
   };
-
-  useFocusEffect(
-    React.useCallback(() => {
-      // This is called when the screen is focused
-      setSearchText("");
-      setFilteredProducts([]);
-    }, [])
-  );
-
-  // Function to pick a random product
-  const pickRandomProduct = () => {
-    const randomIndex = Math.floor(Math.random() * productList.length);
-    return productList[randomIndex];
-  };
-
-  // Effect to set the best deal product when the component mounts
-  useEffect(() => {
-    const randomProduct = pickRandomProduct();
-    setBestDealProduct(randomProduct);
-  }, []);
 
   return (
     <LinearGradient
@@ -238,7 +256,6 @@ function HomeScreen({ navigation }) {
             <Text style={styles.buttonText}>Create List</Text>
           </TouchableOpacity>
         </View>
-
         <SearchBar
           containerStyle={styles.searchcontainer}
           inputContainerStyle={styles.searchinputcontainer}
@@ -247,21 +264,22 @@ function HomeScreen({ navigation }) {
           onChangeText={(text) => handleSearch(text)}
           onSubmitEditing={() => {
             if (searchText.trim() !== "") {
-              // Navigate to SearchScreen only if the search text is not empty
               navigation.navigate("SearchResult", {
                 searchedText: searchText,
                 products: filteredProducts.sort((a, b) => a.p - b.p),
               });
+              setFilteredProducts([]);
+              setSearchText("");
             }
           }}
           value={searchText}
         />
-
         {filteredProducts.length > 0 && (
           <View style={styles.searchdropdownContainer}>
             <FlatList
-              data={filteredProducts.slice(0, 100).sort((a, b) => a.p - b.p)}
+              data={filteredProducts.slice(0, 50).sort((a, b) => a.p - b.p)}
               keyExtractor={(item, index) => index.toString()}
+              numColumns={2}
               renderItem={({ item }) => (
                 <View style={styles.productItem}>
                   <TouchableOpacity
@@ -276,29 +294,32 @@ function HomeScreen({ navigation }) {
             />
           </View>
         )}
-        <View style={styles.dealsContainer}>
-          <Text style={styles.dealconttext}>Best Deals</Text>
-          {bestDealProduct && (
-            <View style={styles.productContainer}>
-              <Text style={styles.productName}>{bestDealProduct.name}</Text>
-              <Image
-                style={styles.productImage}
-                source={{ uri: bestDealProduct.imageUri }}
-              />
-              <Text style={styles.productPrice}>€{bestDealProduct.price}</Text>
+        <View style={styles.bestDealsContainer}>
+          <Text style={styles.bestDealTitle}>Best Deals</Text>
+          {bestDealProducts.length > 0 && (
+            <View style={styles.bestDealsCard}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("Item", {
+                    item: bestDealProducts[currentProductIndex],
+                  });
+                }}
+              >
+                <Card.Image
+                  source={{
+                    uri: bestDealProducts[currentProductIndex].imageURL,
+                  }}
+                  style={styles.bestDealImage}
+                />
+                <Text style={styles.bestDealName}>
+                  {bestDealProducts[currentProductIndex].n}
+                </Text>
+                <Text style={styles.bestDealPrice}>
+                  €{bestDealProducts[currentProductIndex].p.toFixed(2)}
+                </Text>
+              </TouchableOpacity>
             </View>
           )}
-        </View>
-        <View style={styles.dealsContainer}>
-          <Text style={styles.dealconttext}>Best Deals</Text>
-          {/* <FontAwesome name="image" size={256} color="black" /> */}
-          {/* <Image
-            source={{
-              with: 200,
-              height: 300,
-              uri: "https://picsum.photos/200/300",
-            }}
-          /> */}
         </View>
       </View>
     </LinearGradient>
@@ -308,6 +329,11 @@ function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
+  },
+  userName: {
+    backgroundColor: "gray",
+    alignSelf: "flex-end",
+    marginRight: 20,
   },
   title: {
     fontSize: 40,
@@ -367,56 +393,54 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     padding: 2,
   },
-  // dealsContainer: {
-  //   marginTop: 20,
-  //   width: "90%",
-  //   backgroundColor: "#271936",
-  //   borderRadius: 20,
-  //   borderColor: "darkgray",
-  //   padding: 10,
-  //   height: "55%",
-  //   alignSelf: "center",
-  // },
-  dealconttext: {
-    fontSize: 20,
-    color: "white",
-    marginBottom: 20,
-  },
   productName: {
     fontSize: 16,
-    color: "white", //search text color
-  },
-  dealsContainer: {
-    alignItems: "center",
-    marginTop: 50,
     color: "white",
   },
-  dealconttext: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-    color: "white",
-  },
-  productContainer: {
+
+  bestDealsContainer: {
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
     alignItems: "center",
     color: "white",
+    marginTop: 20,
+    borderWidth: 0,
   },
-  productName: {
+  bestDealTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    alignSelf: "flex-start",
+    paddingLeft: 20,
+  },
+  bestDealsCard: {
+    width: "90%",
+    overflow: "hidden",
+    backgroundColor: "#271936",
+    borderRadius: 10,
+    borderWidth: 0,
+    height: 450,
+  },
+  bestDealName: {
     fontSize: 18,
     marginBottom: 5,
     color: "white",
+    paddingLeft: 10,
   },
-  productImage: {
-    width: 200,
-    height: 200,
+  bestDealImage: {
+    width: 350,
+    height: 350,
     marginBottom: 10,
+    marginTop: 10,
+    alignSelf: "center",
+    borderRadius: 10,
   },
-  productPrice: {
-    fontSize: 16,
-    color: "green",
+  bestDealPrice: {
+    fontSize: 24,
+    color: "#C87E61",
+    alignSelf: "flex-end",
+    padding: 10,
   },
 });
 
