a0927ed9e826b53e95606483100c8293da969779
Musti-007 <60880522+Musti-007@users.noreply.github.com>
2023-12-28 01:10:11 +0100

    add routes from user to supermakets

--- Implementation/Frontend/screens/GetDirectionScreen.js
+++ Implementation/Frontend/screens/GetDirectionScreen.js
@@ -1,76 +1,132 @@
 import React, { useState, useEffect, useRef } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import MapView, { Marker, Callout } from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { LinearGradient } from "expo-linear-gradient";
 import { AntDesign, MaterialIcons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import MapView, { Marker, Polyline, Callout } from "react-native-maps";
 
-const GetDirectionScreen = ({ route }) => {
+const GetDirectionScreen = ({ route, navigation }) => {
   const { combinedData } = route.params;
-  const navigation = useNavigation();
   const mapViewRef = useRef(null);
 
-  // Set initial region based on the user's location stored in AsyncStorage
-  const [initialRegion, setInitialRegion] = useState({});
   const [userLocation, setUserLocation] = useState(null);
-  const [currentRegion, setCurrentRegion] = useState(null);
+  const [initialRegion, setInitialRegion] = useState({});
+  const [routes, setRoutes] = useState([]);
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
     const fetchUserLocation = async () => {
       const location = await getUserLocation();
+
+      setUserLocation(location);
       setInitialRegion(location);
 
-      // Animate the map to the user's location
       if (mapViewRef.current && location) {
-        mapViewRef.current.animateToRegion(location, 1000); // Adjust duration as needed
+        mapViewRef.current.animateToRegion(location, 1000);
       }
     };
 
     fetchUserLocation();
-  }, []); // Run once when the component mounts
+  }, []);
 
-  // Function to get the user's location from AsyncStorage
+  const handleLocateMe = () => {
+    if (mapViewRef.current && userLocation) {
+      mapViewRef.current.animateToRegion(userLocation, 1000);
+    }
+  };
+
   const getUserLocation = async () => {
     try {
       const storedLocation = await AsyncStorage.getItem("location");
-      console.log(storedLocation);
       if (storedLocation) {
-        const [latStr, lonStr] = storedLocation.split(", ");
+        const [latStr, lonStr] = storedLocation.split(",");
 
-        // Convert the string values to numbers
         const latitude = parseFloat(latStr);
         const longitude = parseFloat(lonStr);
 
-        setUserLocation({ latitude, longitude });
-
-        return {
-          latitude,
-          longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        };
+        if (!isNaN(latitude) && !isNaN(longitude)) {
+          return {
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          };
+        } else {
+          console.error("Invalid latitude or longitude:", storedLocation);
+        }
       }
     } catch (error) {
       console.error("Error retrieving user location:", error);
     }
 
-    // Return null if there's an error or no stored location
     return null;
   };
 
-  // Function to handle the "Locate Me" button press
-  const handleLocateMe = () => {
-    if (mapViewRef.current && userLocation) {
-      mapViewRef.current.animateToRegion(userLocation, 1000);
-      setCurrentRegion(userLocation);
+  const getRoutePolyline = async () => {
+    try {
+      const routesPromises = combinedData.map(async (item) => {
+        const waypoints = [
+          `${initialRegion.longitude},${initialRegion.latitude}`,
+          `${item.Location.split(",")[1].trim()},${item.Location.split(
+            ","
+          )[0].trim()}`,
+        ];
+
+        const response = await axios.get(
+          `http://router.project-osrm.org/route/v1/driving/${waypoints.join(
+            ";"
+          )}?overview=full&steps=true&geometries=geojson`
+        );
+
+        if (response.data.code === "Ok") {
+          const duration = response.data.routes[0].duration / 60; // Convert seconds to minutes
+          const distance = response.data.routes[0].distance / 1000; // Convert meters to kilometers
+
+          console.log(
+            `Time from user to ${item.StoreName}: ${duration.toFixed(
+              2
+            )} minutes, Distance: ${distance.toFixed(2)} km`
+          );
+
+          return {
+            coordinates: response.data.routes[0].geometry.coordinates.map(
+              (coordinate) => ({
+                latitude: coordinate[1],
+                longitude: coordinate[0],
+              })
+            ),
+            duration: duration.toFixed(2),
+            distance: distance.toFixed(2),
+          };
+        } else {
+          console.error("Error fetching route:", response.data.message);
+          return null;
+        }
+      });
+
+      const routes = await Promise.all(routesPromises);
+
+      // Filter out any routes that had an error
+      const validRoutes = routes.filter((route) => route !== null);
+
+      setRoutes(validRoutes);
+    } catch (error) {
+      console.error("Error fetching routes:", error.message);
     }
   };
 
-  // Function to handle region changes
-  const handleRegionChangeComplete = (region) => {
-    setCurrentRegion(region);
-  };
+  useEffect(() => {
+    if (initialRegion.latitude && combinedData.length > 0) {
+      setIsReady(true); // Set isReady to true when both initialRegion and combinedData are available
+    }
+  }, [initialRegion, combinedData]);
+
+  useEffect(() => {
+    if (isReady) {
+      getRoutePolyline();
+    }
+  }, [isReady]);
 
   return (
     <LinearGradient
@@ -90,32 +146,55 @@ const GetDirectionScreen = ({ route }) => {
         <MapView
           style={styles.map}
           ref={mapViewRef}
-          initialRegion={initialRegion}
+          initialRegion={userLocation}
           showsUserLocation={true}
           showsMyLocationButton={true}
-          onRegionChangeComplete={handleRegionChangeComplete}
           userLocationAnnotationTitle="Your Location"
         >
-          <TouchableOpacity
-            onPress={handleLocateMe}
-            style={styles.locateButton}
-          >
-            <MaterialIcons name="my-location" size={24} color="black" />
+          <TouchableOpacity onPress={handleLocateMe}>
+            <MaterialIcons
+              style={styles.locateButton}
+              name="my-location"
+              size={24}
+              color="black"
+            />
           </TouchableOpacity>
+          {routes.map((route, index) => (
+            <Polyline
+              key={index}
+              coordinates={route.coordinates}
+              strokeWidth={3}
+              strokeColor="blue"
+            />
+          ))}
+
           {combinedData.map((item, index) => {
-            // Split the "lat, lon" string from the Location key
-            const [latStr, lonStr] = item.Location.split(", ");
-
-            // Convert the string values to numbers
-            const latitude = parseFloat(latStr);
-            const longitude = parseFloat(lonStr);
-
+            const route = routes[index]; // Find the corresponding route
             return (
               <Marker
                 key={index}
-                coordinate={{ latitude, longitude }}
+                coordinate={{
+                  latitude: parseFloat(item.Location.split(",")[0]),
+                  longitude: parseFloat(item.Location.split(",")[1]),
+                }}
                 title={item.StoreName}
-              />
+              >
+                <Callout>
+                  <View>
+                    <Text>{item.StoreName}</Text>
+                    <Text>
+                      Estimated Time:{" "}
+                      {route && route.duration
+                        ? Math.round(route.duration) + " minutes"
+                        : "N/A"}
+                    </Text>
+                    <Text>
+                      Distance:{" "}
+                      {route && route.distance ? route.distance + " km" : "N/A"}
+                    </Text>
+                  </View>
+                </Callout>
+              </Marker>
             );
           })}
         </MapView>
